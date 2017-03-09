888ec164923fced839a753f91a8b015bf94027a6
losaped <losaped@gmail.com>
2017-03-09 17:58:10 +0300

    Изменил получение одного файла на список

--- service.go
+++ service.go
@@ -10,6 +10,7 @@ import (
 	"github.com/garyburd/redigo/redis"
 	"github.com/weekface/mgorus"
 	mgo "gopkg.in/mgo.v2"
+	"strings"
 )
 
 type service struct {
@@ -87,7 +88,7 @@ func (svc *service) Run() {
 				case <-svc.queueDone:
 					return
 				default:
-					svc.processFile(routineNum, svc.fileFromQueue)
+					svc.processFilesList(routineNum, svc.fileListQueue)
 				}
 			}
 		}(i)
@@ -113,7 +114,7 @@ func (svc *service) ProcessErrors() {
 				case <-svc.errorsDone:
 					return
 				default:
-					svc.processFile(routineNum, svc.fileFromErrors)
+					svc.processFile(routineNum, svc.fileListFromErrors)
 				}
 			}
 		}(i)
@@ -127,70 +128,77 @@ func (svc *service) ProcessErrors() {
 // указывая  тип ошибки, соответствующий этапу обработки. После чего выходим из функции.
 // Если ни на одном из этапов обработки ошибок не возникло, то пытаемся удалить файл с диска.
 // В случае неудачи так же запишем сообщение в очередь ошибок с типом ErrorRemove
-func (svc *service) processFile(routineNum int, getFileFunc func() (string, error)) {
-	str, err := getFileFunc()
+func (svc *service) processFilesList(routineNum int, getListFunc func() (string, error)) {
+	queueList, err := getListFunc()
 	if err != nil {
 		svc.log().Warning("Нет файлов в очереди")
 		time.Sleep(1 * time.Minute)
 		return
 	}
 
-	svc.log().Infof("Routine %d: Файл из очереди: %s\n", routineNum, str)
-	xmlBts, err := ioutil.ReadFile(str)
-	if err != nil {
-		svc.storeFileProcessError(ErrorReadFile, str, err)
-		svc.Errorf("Routine %d: не удалось прочесть файл: %v\n", routineNum, err)
-		return
-	}
+	list := strings.Split(queueList, ",")
+	for _, str := range list {
+		svc.log().Infof("Routine %d: Файл из очереди: %s\n", routineNum, str)
+		xmlBts, err := ioutil.ReadFile(str)
+		if err != nil {
+			svc.storeFileProcessError(ErrorReadFile, str, err)
+			svc.Errorf("Routine %d: не удалось прочесть файл: %v\n", routineNum, err)
+			return
+		}
 
-	ei, err := getExportInfo(string(xmlBts))
-	if err != nil {
-		svc.storeFileProcessError(ErrorExportInfo, str, err)
-		svc.log().Errorf("Routine %d: Не удалось прочесть версию и коллекцию из файла %s: %v\n", routineNum, str, err)
-		return
-	}
+		ei, err := getExportInfo(string(xmlBts))
+		if err != nil {
+			svc.storeFileProcessError(ErrorExportInfo, str, err)
+			svc.log().Errorf("Routine %d: Не удалось прочесть версию и коллекцию из файла %s: %v\n", routineNum, str, err)
+			return
+		}
 
-	cli := GetClient()
-	url := svc.GetServiceURL(ei.Version)
-	err = cli.SendData(url, xmlBts)
-	if err != nil {
-		svc.storeFileProcessError(ErrorSend, str, err)
-		svc.log().Errorf("Routine %d: Ошибка при отправке данных на сервис %s: %v\n", routineNum, url, err)
-		return
-	}
+		cli := GetClient()
+		url := svc.GetServiceURL(ei.Version)
+		err = cli.SendData(url, xmlBts)
+		if err != nil {
+			svc.storeFileProcessError(ErrorSend, str, err)
+			svc.log().Errorf("Routine %d: Ошибка при отправке данных на сервис %s: %v\n", routineNum, url, err)
+			return
+		}
 
-	svc.log().Info("Пытаемся удалить обработанный и сохраненный файл: ", str)
-	if err := os.Remove(str); err != nil {
-		svc.storeFileProcessError(ErrorRemove, str, err)
-		svc.log().Errorf("Routine %d: Не удалось удалить файл: %v", routineNum, err)
-	} else {
-		svc.log().Infoln("Файл удалён")
+		svc.log().Info("Пытаемся удалить обработанный и сохраненный файл: ", str)
+		if err := os.Remove(str); err != nil {
+			svc.storeFileProcessError(ErrorRemove, str, err)
+			svc.log().Errorf("Routine %d: Не удалось удалить файл: %v", routineNum, err)
+		} else {
+			svc.log().Infoln("Файл удалён")
+		}
 	}
 }
 
 // Получаем путь к файлу из очереди сервиса загрузки
-func (svc *service) fileFromQueue() (string, error) {
+func (svc *service) fileListQueue() (string, error) {
 	conn := svc.redisPool.Get()
 	defer conn.Close()
 	return redis.String(conn.Do("RPOP", "FileQueue"))
 }
 
 // Получаем путь к файлу из очереди ошибок
-func (svc *service) fileFromErrors() (string, error) {
-	pe := &ProcessError{}
+func (svc *service) fileListFromErrors() (string, error) {
+	pe := []ProcessError{}
 	mErr := svc.mongoExec(svc.ErrorCollection, func(col *mgo.Collection) error {
-		return col.Find(nil).One(pe)
+		return col.Find(nil).All(&pe)
 	})
 
 	if mErr != nil {
 		return "", mErr
 	}
 
-	svc.mongoExec(svc.ErrorCollection, func(col *mgo.Collection) error {
-		return col.RemoveId(pe.ID)
-	})
-
-	return pe.FilePath, nil
+	//TODO: Сделать удаление обработанных
+	// svc.mongoExec(svc.ErrorCollection, func(col *mgo.Collection) error {
+	// 	return col.RemoveId(pe.ID)
+	// })
+	fileList := make([]string, 0, len(pe))
+	for i, p := range pe {
+		fileList[i] = p.FilePath
+	}
+	return strings.Join(fileList, ","), nil
 }
 
 // Сохраним информацию об ошибке обработки файла в очередь для последующей обработки
